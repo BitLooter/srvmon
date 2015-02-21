b93eb0ba99e6667a3f79425379538c45a87dae30
David Powell <pwnzerdragoon+gh@gmail.com>
2015-02-21 11:32:37 -0800

    Displays free disk space of volumes in displaylist

--- srvmon.py
+++ srvmon.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
 
+import os
 from flask import Flask, render_template
 app = Flask(__name__)
 
+class VolumeInfo:
+    def __init__(self, volume):
+        #TODO: Verify volume is valid
+        self.volume = volume.strip()
+        self._stat = os.statvfs(self.volume)
+
+    @property
+    def free_space(self):
+        return self._stat.f_bavail * self._stat.f_frsize
+
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    display_list_file = open("displaylist", 'r')
+    render_items = []
+    for line in display_list_file:
+        info = VolumeInfo(line)
+        render_items.append(info)
+
+    return render_template('index.html', render_items=render_items)
 
 if __name__ == "__main__":
     app.debug = True
