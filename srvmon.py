36679469ad0b2e936b88e7aebac2ae15d435b5f2
David Powell <pwnzerdragoon+gh@gmail.com>
2015-02-26 08:28:16 -0800

    Display list parsing moved into a class

--- srvmon.py
+++ srvmon.py
@@ -14,15 +14,26 @@ class VolumeInfo:
     def free_space(self):
         return self._stat.f_bavail * self._stat.f_frsize
 
+    @property
+    def total_size(self):
+        return self._stat.f_blocks * self._stat.f_frsize
+
+class DisplayList(list):
+    def __init__(self, display_list_path):
+        list.__init__(self)
+
+        self._parse_display_list(display_list_path)
+
+    def _parse_display_list(self, display_list_path):
+        list_file = open(display_list_path, 'r')
+        for line in list_file:
+            self.append(VolumeInfo(line))
+
 @app.route("/")
 def hello():
-    display_list_file = open("displaylist", 'r')
-    render_items = []
-    for line in display_list_file:
-        info = VolumeInfo(line)
-        render_items.append(info)
+    display_list = DisplayList("displaylist")
 
-    return render_template('index.html', render_items=render_items)
+    return render_template('index.html', render_items=display_list)
 
 if __name__ == "__main__":
     app.debug = True
