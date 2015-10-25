10435f075b206ad03630ec5cc21cc5274f70da20
David Powell <pwnzerdragoon+gh@gmail.com>
2015-10-25 06:02:24 -0700

    Added some functions for testing

--- sysinfo/__init__.py
+++ sysinfo/__init__.py
@@ -7,4 +7,8 @@ from sysinfo.linuxinfo import *
 displaylist_functions = {
     'VolumeFreeSpace': lambda x: VolumeInfo(x).free_space,
     'VolumeTotalSpace': lambda x: VolumeInfo(x).total_size,
+
+    # Dummy functions used for testing
+    '_TestNull': lambda x: None,
+    '_TestEcho': lambda x: x,
 }
