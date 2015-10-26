0852d38766cc8eddc27368e345e26ff4ac40f9b4
David Powell <pwnzerdragoon+gh@gmail.com>
2015-10-26 09:26:15 -0700

    Created config file

---
+++ config/defaults.py
@@ -0,0 +1,8 @@
+"""Default configuration settings go here"""
+
+# Flask settings
+PORT = 8080
+
+# Program settings
+DISPLAYLIST = 'parsertest'
+THEME = 'default'
--- srvmon.py
+++ srvmon.py
@@ -7,10 +7,11 @@ from displaylist import DisplayList, DisplayListError
 
 #TODO: Change this when a name for the program is decided
 app = flask.Flask(__name__)
+app.config.from_object('config.defaults')
 
 @app.route("/")
 def index():
-    display_list = DisplayList("displaylist")
+    display_list = DisplayList(app.config['DISPLAYLIST'])
     return flask.render_template('index.html', render_items=display_list)
 
 @app.errorhandler(DisplayListError)
@@ -20,4 +21,4 @@ def displaylist_error(error):
 if __name__ == "__main__":
     app.debug = True
     #TODO: Define port with command line option
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=app.config['PORT'])
